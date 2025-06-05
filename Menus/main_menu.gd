fb3db47ebe92ecc4bc664a0bc649a957a3378777
WolfQuery <qwertman783@outlook.com>
2025-06-06 00:22:43 +0200

    Finished prototype of title screen & added some free for use fonts

---
+++ Menus/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+# main menu script
+
+
+func _on_play_button_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_button_up() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_button_up() -> void:
+	get_tree().quit()
